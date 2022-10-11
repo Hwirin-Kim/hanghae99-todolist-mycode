51ee3f2ca5ca4efb57ad475e3caa0dea7797f7e2
Hwirin-Kim <kimhuirin@gmail.com>
2022-10-11 14:05:42 +0900

    주석삭제

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,9 @@
-import React, { useState } from "react";
-
 import Header from "../header/Header";
 import Form from "../form/Form";
 import List from "../list/List";
 import styled from "styled-components";
 
 const Layout = () => {
-  //투두리스트에서 사용할 리스트들을 저장할 상태를 todos란 스테이트로 선언
-  // const [todos, setTodos] = useState([]);
-
   const StLayout = styled.div`
     max-width: 1200px;
     min-width: 800;
